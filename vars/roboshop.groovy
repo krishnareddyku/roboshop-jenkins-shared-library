a515ae32c1b16809bdb3557e90157aed9a3b969c
Cloud User <centos@ip-172-31-56-104.ec2.internal>
2023-01-26 13:54:11 +0000

    updated

---
+++ vars/roboshop.groovy
@@ -0,0 +1,33 @@
+def call {
+
+pipeline {
+	agent {
+	label 'kk'
+	}
+	
+	stages {
+		stage('compile/build') {
+			steps {
+			echo "compile"
+			}
+		}
+		stage('Unit tests') {
+                        steps {
+                        echo "unit tests"
+                        }
+                }
+		stage('Quality Control') {
+                        steps {
+                        echo "Quality control"
+                        }
+                }
+		stage('upload code to centralized place') {
+                        steps {
+                        echo "upload code to centralized place"
+                        }
+                }
+	}
+
+}
+
+}
