26075fdc3dbca708daf540d3a39eb9fd4fc81c90
krishnareddyku <krishnareddyku@gmail.com>
2023-01-29 16:16:01 +0530

    updated

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -1,34 +1,38 @@
 def call () {
+  try {
+     pipeline {
 
-  pipeline {
+         agent any
 
-    agent any
+         stages {
+             stage('compile/build') {
+                 steps {
+                     script {
+                         common.compile()
+                     }
+                 }
+             }
+             stage('Unit tests') {
+                 steps {
+                     scrit {
+                         common.unittests()
+                     }
+                 }
+             }
+             stage('Quality Control') {
+                 steps {
+                     echo "Quality control"
+                 }
+             }
+             stage('upload code to centralized place') {
+                 steps {
+                     echo "upload code to centralized place"
+                 }
+             }
+         }
 
-	stages {
-		stage('compile/build') {
-			steps {
-			  script {
-               common.compile()
-              }
-			}
-		}
-		stage('Unit tests') {
-                        steps {
-                        echo "unit tests"
-                        }
-        }
-		stage('Quality Control') {
-                        steps {
-                        echo "Quality control"
-                        }
-        }
-		stage('upload code to centralized place') {
-                        steps {
-                        echo "upload code to centralized place"
-                        }
-        }
-	}
-
+     }
+  } catch(Exception e) {
+      mail bcc: '', body: 'TEST', cc: '', from: 'kkr@bcmonline.com', replyTo: '', subject: 'test from jenkins', to: 'myteam@bcmonline.com'
   }
-
 }
--- vars/common.groovy
+++ vars/common.groovy
@@ -1,13 +1,36 @@
 def compile() {
     if (app_lang == "node.js") {
-        echo "npm install"
+        sh 'npm install'
     }
     if (app_lang == "maven") {
-        sh  "mvn package"
+        sh  'mvn package'
     }
     if (app_lang == "go") {
-        sh  "go mod init dispatch"
-        sh  "go get"
-        sh "go build"
+        sh  'go mod init dispatch'
+        sh  'go get'
+        sh 'go build'
     }
+}
+
+def unittests() {
+
+    if (app_lang == "node.js") {
+        // Developer is missing unit test cases in our project, we are forcefully passing the test
+        // To report this issue to dev team; we need to use "exception handling" with try/catch groovy..
+      sh 'npm test'
+    }
+    if (app_lang == "maven") {
+        sh 'mvn test'
+    }
+    if (app_lang == "go") {
+        sh  'go mod init dispatch'
+    }
+    if (app_lang == "python") {
+        sh  'python3 -m unittest'
+    }
+
+}
+
+def email(emial_note) {
+    println email_note
 }
