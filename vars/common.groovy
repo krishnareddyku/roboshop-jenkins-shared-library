4b93120421b893d74bdcbb6eae91abc5e0586c04
krishnareddyku <krishnareddyku@gmail.com>
2023-01-29 15:20:25 +0530

    updated

--- vars/common.groovy
+++ vars/common.groovy
@@ -5,4 +5,9 @@ def compile() {
     if (app_lang == "maven") {
         sh  "mvn package"
     }
+    if (app_lang == "go") {
+        sh  "go mod init dispatch"
+        sh  "go get"
+        sh "go build"
+    }
 }
